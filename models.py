4b5e1c51e688940f6966f172aaab9ce8613baa00
HandsomeHan <HandsomeHan515@163.com>
2018-01-26 14:38:09 +0800

    add refund model

--- models.py
+++ models.py
@@ -15,3 +15,22 @@ class Order(models.Model):
 
     def __str__(self):
         return self.subject
+
+class Refund(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
+    out_trade_no = models.CharField(max_length=64,  verbose_name="商户订单号")
+    trade_no = models.CharField(
+        max_length=64, null=True, blank=True, verbose_name='支付宝系统中的交易流水号')
+    out_refund_no = models.CharField(
+        max_length=64, null=True, blank=True, verbose_name="商户退款单号")
+    refund_amount = models.FloatField(
+        verbose_name="退款金额", help_text="订单总金额，单位元")
+    refund_reason = models.CharField(
+        max_length=128, null=True, blank=True, verbose_name='退款原因')
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=32, default="0", verbose_name="状态", choices=(
+        ("1", "完成"), ("0", "未完成")))
+
+    def __str__(self):
+        return self.out_trade_no
