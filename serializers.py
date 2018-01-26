edd8c48c0fb4fb6b750002fd880e59d6be16fb9c
HandsomeHan <HandsomeHan515@163.com>
2018-01-26 14:38:48 +0800

    add refund serailizer

--- serializers.py
+++ serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Order
+from .models import Order, Refund
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
+        fields = '__all__'
+
+        
+class RefundSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Refund
         fields = '__all__'
