cccb14ad41106462b2f93641760109f948bc5ab6
HandsomeHan <HandsomeHan515@163.com>
2018-01-26 14:40:09 +0800

    add refundview

--- views.py
+++ views.py
@@ -14,10 +14,10 @@ from .utils import urlencode_data as urlencode_data_func
 from .utils import check_sign as check_sign_func
 from .utils import check_ali_sign as check_ali_sign_func
 
-from .config import NOTIFY_URL, APP_ID, RSA_PRIVATE, ALIPAY_PUBLIC_KEY
+from .config import NOTIFY_URL, APP_ID, RSA_PRIVATE, ALIPAY_PUBLIC_KEY, PAY_URL
 
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, Refund
+from .serializers import OrderSerializer, RefundSerializer
 
 
 # 获取签名
@@ -105,3 +105,61 @@ class PayNotifyView(GenericAPIView):
                 order.save()
 
         return Response('success')  # 有效数据需要返回 'success' 给 alipay
+    
+class RefundView(GenericAPIView):
+    serializer_class = RefundSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # user = self.request.user
+        instance = Refund.objects.create(**request.data)
+        instance.out_refund_no = 'alipay-refund' + timezone.now().strftime('%Y%m%d') + \
+            '%s' % instance.id
+        instance.save()
+
+        out_trade_no = serializer.validated_data['out_trade_no']
+        refund_amount = serializer.validated_data['refund_amount']
+        refund_reason = serializer.validated_data.get('refund_reason')
+
+        biz_content = {
+            'refund_reason': refund_reason,
+            'refund_amount': '%.2f' % float(refund_amount),
+            'out_trade_no': out_trade_no,
+        }
+
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        app_id = settings.ALIPAY['APP_ID']
+
+        payload = {
+            'app_id': app_id,
+            'method': 'alipay.trade.refund',
+            'charset': 'utf-8',
+            'sign_type': 'RSA2',
+            'timestamp': timestamp,
+            'version': '1.0',
+            'biz_content': json.dumps(biz_content, separators=(',', ':'))
+        }
+
+        private_key = settings.RSA_PRIVATE
+
+        # 对payload转化成key+value&对，并且排序
+        order_payload = order_data(payload)
+
+        # 根据排序好的数据获取签名
+        sign = sign_func(order_payload, private_key)
+
+        urlencode_data = urlencode_data_func(payload, sign)
+
+        url = PAY_URL.replace('payload', urlencode_data)
+
+        req_obj = requests.get(url).json()
+        if req_obj['alipay_trade_refund_response']['code'] == '10000':
+            instance.status = "1"
+            instance.trade_no = req_obj['alipay_trade_refund_response']['trade_no']
+            instance.save()
+
+        # print('req_obj: {}'.format(req_obj))
+        return Response({'message': 'ok'})
